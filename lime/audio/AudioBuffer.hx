0040f24f3101c2bcd8539a155d8d924829097dc3
James Gray <james@james4k.com>
2015-12-26 23:50:12 -0600

    console: rename fmod.Sound to fmod.FMODSound

--- lime/audio/AudioBuffer.hx
+++ lime/audio/AudioBuffer.hx
@@ -12,7 +12,7 @@ import js.html.Audio;
 #elseif flash
 import flash.media.Sound;
 #elseif lime_console
-import lime.audio.fmod.Sound;
+import lime.audio.fmod.FMODSound;
 #end
 
 #if !macro
@@ -34,7 +34,7 @@ class AudioBuffer {
 	#elseif flash
 	public var src:Sound;
 	#elseif lime_console
-	public var src:Sound;
+	public var src:FMODSound;
 	private var __root:Dynamic;
 	#else
 	public var src:Dynamic;
@@ -103,7 +103,7 @@ class AudioBuffer {
 		
 		#if lime_console
 		
-		var sound:Sound = Sound.fromFile (path);
+		var sound:FMODSound = FMODSound.fromFile (path);
 		
 		if (sound.valid) {
 			
